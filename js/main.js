7a504d8720a304dee020945f1133eafc6da0a0cb
agian <angelos.giannourakos@gmail.com>
2022-04-26 23:26:17 +0300

    somechanges in js and css

--- js/main.js
+++ js/main.js
@@ -11,31 +11,76 @@ navigator.geolocation.getCurrentPosition((position) => {
          .then(res => res.json()) // parse response as JSON
          .then(data => {
            console.log(data)
-           let tempCelsius = data.main.temp - 273.15
+           let tempCelsius        = data.main.temp - 273.15
            let weatherDescription = data.weather[0].description 
-           let descriptionType = data.weather[0].main 
-           let location    = data.name
-           let country     = data.sys.country
+          //  let descriptionType    = data.weather[0].main
+           let humidity           = data.main.humidity
+           let location           = data.name
+           let country            = data.sys.country
            document.querySelector('#location').innerText = location;
            document.querySelector('#country').innerText = country;
            document.querySelector('#temp').innerText = tempCelsius.toFixed(0);
            document.querySelector('#description').innerText = weatherDescription;
-           document.querySelector('#description-type').innerText = descriptionType;
-
+           document.querySelector('#humidity').innerText = humidity;
+          //  document.querySelector('#humidity').appendChild('em')
+          //  document.querySelector('#description').innerText = descriptionType;
+          // let humidSelector = document.querySelector('#humidity')
+ 
            let icon = document.createElement('em')
+          //  document.querySelector('#humidity').appendChild(icon).classList.add('wi-humidity')
            icon.classList.add('wi')
-        //    document.querySelector('#description-type').appendChild(icon);
 
-           if(descriptionType == "Clouds"){
-            icon.classList.add('wi-cloudy')      
-            document.querySelector('#description-type').appendChild(icon) 
-          } else if(descriptionType == "Clear"){
-             icon.classList.add('wi-day-sunny')
-             document.querySelector('#description-type').appendChild(icon)
-           }
+          switch(weatherDescription){
+            case 'clear sky':
+              document.querySelector('body').classList.add('clearskies');
+              document.querySelector('#description').appendChild(icon).classList.add('wi-day-sunny')
+              break;
+            case 'few clouds':
+              document.querySelector('body').classList.add('few-clouds');
+              document.querySelector('#description').appendChild(icon).classList.add('wi-cloud')
+              break;
+            case 'few clouds':
+              document.querySelector('body').classList.add('skattered-clouds');
+              icon.classList.add('wi-cloudy')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'broken clouds':
+              document.querySelector('body').classList.add('skattered-clouds');
+              icon.classList.add('wi-cloudy')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'shower rain':
+              document.querySelector('body').classList.add('shower-rain');
+              icon.classList.add('wi-showers')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'rain':
+              document.querySelector('body').classList.add('rain');
+              icon.classList.add('wi-rain')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'thunderstorm':
+              document.querySelector('body').classList.add('thunderstorm');
+              icon.classList.add('wi-thunderstorm')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'snow':
+              document.querySelector('body').classList.add('snow');
+              icon.classList.add('wi-snow')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            case 'mist':
+              document.querySelector('body').classList.add('mist');
+              icon.classList.add('wi-fog')      
+              document.querySelector('#description').appendChild(icon)
+              break;  
+            default:
+              document.querySelector('#description').innerText = 'Not defined'
+          }
 
 
-           console.log(weatherDescription)
+
+      
          })
          .catch(err => {
              console.log(`error ${err}`)
